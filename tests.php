d7b0c200e7a79de4e8a285017cc9b328bb0b5a57
Eric Pruitt <eric.pruitt@gmail.com>
2014-03-23 18:25:43 -0500

    Less nesting

--- tests.php
+++ tests.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
-require('src/Codevat/Set.php');
-require('src/Codevat/SetTests.php');
+require('src/Set.php');
+require('src/SetTests.php');
 
 $suite = new \Codevat\SetTests();
 $suite->runTests();
